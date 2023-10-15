ccaa4b182d9f8822e25335b516cb13a61ff0ae99
Samed Kütahyalı <smdkthyli@gmail.com>
2023-10-15 16:58:19 +0300

    Redis implementation has been added.

--- src/rentACar/WebApi/Program.cs
+++ src/rentACar/WebApi/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 
-builder.Services.AddDistributedMemoryCache();
+//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379");
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
